63ffebfadbb79cb9d6d4361a1fff77700bc10d08
seohoo <63957830+seohoo@users.noreply.github.com>
2020-12-23 16:59:25 +0900

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,15 @@
+const int led = 13;
+const int btn = 2;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Setuped");
+}
+
+void loop() {
+  /*zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+   * zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+   */
+}
